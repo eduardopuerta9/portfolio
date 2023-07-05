caad258aa4bc276e4ab45ade44dec96972401944
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-07-05 09:18:59 -0400

    added profile image

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,13 @@
+import '../App.css'
 import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <section className="startSection">
-      <h1 className="WelcomeMessage">Eduardo Puerta's Portfolio</h1>
+      <img
+        className="profile"
+        src="https://media.licdn.com/dms/image/D4E03AQFgPxhRXJqV7g/profile-displayphoto-shrink_800_800/0/1681248097796?e=1694044800&v=beta&t=LeEWfd99eLiBlJFbXKkq1as4HO9e0ud6ydQbGTo7ctI"
+      ></img>
+      <h3 className="greeting">Welcome!</h3>
     </section>
   )
 }
